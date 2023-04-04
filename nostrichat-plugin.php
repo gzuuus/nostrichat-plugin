ae1e030b30a7a9ceafb7de0ab63b45f28b0a6e41
gzuuus <gzuuus@protonmail.com>
2023-04-04 15:41:39 +0200

    first commit

---
+++ nostrichat-plugin.php
@@ -0,0 +1,91 @@
+<?php
+/*
+Plugin Name: Nostrichat Plugin
+Description: This plugin inserts <a href="https://nostri.chat/" target="_blank" rel="noreferrer">Nostrichat</a> by <a href="https://snort.social/p/npub1l2vyh47mk2p0qlsku7hg0vn29faehy9hy34ygaclpn66ukqp3afqutajft"target="_blank" rel="noreferrer">PABLOF7z</a> in your posts or pages trought the shortcode [nostrichat].
+Version: 1.0
+Author: <a href="https://snort.social/p/npub1gzuushllat7pet0ccv9yuhygvc8ldeyhrgxuwg744dn5khnpk3gs3ea5ds" target="_blank" rel="noreferrer">@Gzuuus</a>
+*/
+
+function insert_nostrichat_shortcode($atts) {
+    $atts = shortcode_atts( array(
+        'chat-type' => 'GLOBAL',
+    ), $atts );
+    $pubkey = get_option('nostrichat_pubkey');
+    $relays = get_option('nostrichat_relays');
+    $url = get_permalink();
+    $shortcode = '<script
+        src="https://nostri.chat/public/bundle.js"
+        data-website-owner-pubkey="' . esc_attr($pubkey) . '"
+        data-chat-type="' . esc_attr($atts['chat-type']) . '"
+        data-chat-tags="' . esc_attr($url) . '"
+        data-relays="' . esc_attr($relays) . '"
+        style="z-index: 99;"
+    ></script>
+    <link rel="stylesheet" href="https://nostri.chat/public/bundle.css">';
+    return $shortcode;
+}
+add_shortcode( 'nostrichat', 'insert_nostrichat_shortcode' );
+
+function nostrichat_add_settings_page() {
+    add_options_page( 'Nostrichat Settings', '🦩Nostrichat', 'manage_options', 'nostrichat', 'nostrichat_settings_page' );
+}
+add_action( 'admin_menu', 'nostrichat_add_settings_page' );
+
+function nostrichat_settings_page() {
+    $pubkey = get_option('nostrichat_pubkey');
+    $relays = get_option('nostrichat_relays');
+    ?>
+    <div class="wrap">
+        <h1>Nostrichat Settings</h1>
+        <form method="post" action="options.php">
+            <?php settings_fields( 'nostrichat_options_group' ); ?>
+            <?php do_settings_sections( 'nostrichat_options_group' ); ?>
+            <table class="form-table">
+                <tr>
+                    <th scope="row"><label for="nostrichat_pubkey">Nostr Public Key (HEX)</label></th>
+                    <td><input type="text" id="nostrichat_pubkey" name="nostrichat_pubkey" value="<?php echo esc_attr($pubkey); ?>" /></td>
+                </tr>
+                <tr>
+                    <th scope="row"><label for="nostrichat_relays">Relays (can be a comma-separated list of relays)</label></th>
+                    <td><input type="text" id="nostrichat_relays" name="nostrichat_relays" value="<?php echo esc_attr($relays); ?>" /></td>
+                </tr>
+            </table>
+            <?php submit_button(); ?>
+        </form>
+        <div class="usage">
+            <h3>Basic usage</he>
+            <p> To use this plugin, you must first configure your public key fields and the list of relays. </p>
+            <p> Once these are filled in, you can simply use the following shortcode on any wordpress page: <code>[nostrichat]</code>.</p>
+            <p> You can also use the following argument within the shortcode to specify the chat type 'chat-type=" "', supported values are 'GLOBAL' and 'DM'.</p>
+            <p>💡<i> If you add the shortcode without specifying this argument, the default chat type will be 'GLOBAL'.</i></p>
+            <p>Example shortcode for dm: <code>[nostrichat chat-type="DM"]</code>.</p>
+        </div>
+        <div class="donation">
+            <h3>How to support</h3>
+            <p>If you liked this plugin and found it valuable, please consider supporting the work of the developers.</p>
+            <p>Gzuuus (Wordpress plugin) <a href="lightning:gzuuus@getalby.com">DONATE</a></p>
+            <p>PABLOF7z (Nostrichat) <a href="lightning:pablof7z@ln.tips">DONATE</a></p>
+            <h4>You can also contribute by developing this plugin, opening issues to fix bugs or suggest improvements, opening PRs to commit new changes, etc.</h4>
+            <a href="https://github.com/gzuuus/nostrichat-plugin" target="_blank" rel="noreferrer">Github repo</a>
+    </div>
+    <?php
+}
+
+function nostrichat_register_settings() {
+    register_setting( 'nostrichat_options_group', 'nostrichat_pubkey' );
+    register_setting( 'nostrichat_options_group', 'nostrichat_relays' );
+}
+add_action( 'admin_init', 'nostrichat_register_settings' );
+
+function nostrichat_settings_link($links) {
+    $settings_link = '<a href="admin.php?page=nostrichat">' . __('Settings') . '</a>';
+    array_push($links, $settings_link);
+    return $links;
+}
+add_filter("plugin_action_links_" . plugin_basename(__FILE__), "nostrichat_settings_link");
+
+function nostrichat_uninstall_plugin() {
+    delete_option('nostrichat_pubkey');
+    delete_option('nostrichat_relays');
+}
+register_uninstall_hook( __FILE__, 'nostrichat_uninstall_plugin' );
---
+++ readme.txt
@@ -0,0 +1,21 @@
+=== Nostrichat Plugin ===
+Contributors: Gzuuus (Wordpress plugin) Pablo7zf (Nostrichat development)
+Version: 1.0.0
+Licence: ?
+Licence URI: ?
+
+Description
+The Nostrichat plugin allows you to easily integrate Nostrichat into your WordPress site. With this plugin, you can add a live chat widget to your pages or posts, and allow site visitors to communicate with you in real time.
+
+Basic usage
+To use this plugin, you must first configure your public key fields and the list of relays in the settings page "WordPress admin dashboard > Settings (right sidebar) > Nostrichat".
+Once these are filled in, you can simply use the following shortcode on any wordpress page: [nostrichat].
+You can also use the following argument within the shortcode to specify the chat type 'chat-type=" "', supported values are 'GLOBAL' and 'DM'.
+💡 If you add the shortcode without specifying this argument, the default chat type will be 'GLOBAL'.
+Example shortcode for dm: [nostrichat chat-type="DM"].
+
+== Changelog ==
+
+= 1.0.0 =
+
+Initial version of the plugin
